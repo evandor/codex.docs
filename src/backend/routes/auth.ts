3a2f74c2fbdc9ab18cbe25654e8321422f23b0c0
Carsten Gräf <evandor@gmail.com>
2024-08-22 08:57:59 +0200

    samesite=none cookie setting

--- src/backend/routes/auth.ts
+++ src/backend/routes/auth.ts
@@ -4,7 +4,7 @@ import csrf from 'csurf';
 import appConfig from '../utils/appConfig.js';
 
 const router = express.Router();
-const csrfProtection = csrf({ cookie: true });
+const csrfProtection = csrf({ cookie: {sameSite: 'none'} });
 const parseForm = express.urlencoded({ extended: false });
 
 /**
